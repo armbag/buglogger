22eae1b3aa85c55fd98323c8b689b7c1cbd7dc8a
Armen <armen_bagramian@gmail.com>
2020-06-17 15:45:01 -0400

    basic ui

--- src/components/LogItem.js
+++ src/components/LogItem.js
@@ -1,15 +1,37 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Badge from 'react-bootstrap/Badge'
 
-const LogItem = (props) => {
-	const { user, text, priority, created } = props.detail
+const LogItem = ({ log: { priority, text, user, created } }) => {
+	const setVariant = () => {
+		if (priority === 'high') {
+			return 'danger'
+		} else if (priority === 'moderate') {
+			return 'warning'
+		} else {
+			return 'success'
+		}
+	}
+	console.log(priority.slice(1))
 
 	return (
 		<tr>
-			<td>{priority}</td>
+			<td>
+				<Badge>priority</Badge>
+			</td>
+			<td>
+				<Badge variant={setVariant()} className="p-2">
+					{priority.charAt(0).toUpperCase() + priority.slice(1)}
+				</Badge>
+			</td>
 			<td>{text}</td>
 			<td>{user}</td>
 			<td>{created}</td>
-			<td>{}</td>
+			<td>
+				<Button variant="danger" size="sm">
+					x
+				</Button>
+			</td>
 		</tr>
 	)
 }
