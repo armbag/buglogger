6744f5bcdee283f83c6c40b8f97a9cad35e82343
Armen <armen_bagramian@gmail.com>
2020-06-19 18:31:12 -0400

    Done

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(
-			'mongodb+srv://armen1234:armen1234@cluster0-0amgv.mongodb.net/buglogger?retryWrites=true&w=majority',
+			'mongodb+srv://armen1234:armen1234@cluster0-0amgv.mongodb.net/bugloggergit?retryWrites=true&w=majority',
 			{
 				useNewUrlParser: true,
 				useCreateIndex: true,
